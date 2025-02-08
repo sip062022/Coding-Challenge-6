6fa23e75c85bf16eb5d6bab09928058ee8addd30
Sahara Sipple <sahara36@usf.edu>
2025-02-08 10:53:47 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -13,5 +13,15 @@ function calculateSalesTax(amount, taxRate) { // Declares function and its varia
     return (amount*taxRate) // Defines formula to be used
 };
 
-console.log(`Sales Tax: $${Math.floor(calculateSalesTax(100, 0.07))}`) // Expected output: "Sales Tax: $7" ... used mathfloor to round down the answer to an even $7
-console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`) // Expected output: "Sales Tax: $50"
+console.log(`Sales Tax: $${Math.floor(calculateSalesTax(100, 0.07))}`); // Expected output: "Sales Tax: $7" ... used mathfloor to round down the answer to an even $7
+console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`); // Expected output: "Sales Tax: $50"
+
+// Task 3: Arrow Function //
+
+const calculateBonus = (salary, performanceRating) =>{ // Declares a function and its variables
+    const ratings = { Excellent: 0.20, Good: 0.10, Average: 0.05 }; // Defines the ratings and their numberical equivalents
+    return salary * (ratings[performanceRating]) // Defines the formula to be used
+};
+
+console.log(`Bonus: $${calculateBonus(5000, "Excellent")}`); // Expected output: "Bonus: $1000"
+console.log(`Bonus: $${calculateBonus(7000, "Good")}`); // Expected output: "Bonus: $700"
